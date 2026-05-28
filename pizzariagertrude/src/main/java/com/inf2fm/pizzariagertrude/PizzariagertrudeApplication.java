519741a533c27288defac94c9922a47454584c28
jaogomes09 <rm99703@estudante.fieb.edu.br>
2026-05-28 07:34:28 -0300

    criando ambiente Dev

---
+++ pizzariagertrude/src/main/java/com/inf2fm/pizzariagertrude/PizzariagertrudeApplication.java
@@ -0,0 +1,13 @@
+package com.inf2fm.pizzariagertrude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariagertrudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariagertrudeApplication.class, args);
+	}
+
+}
---
+++ pizzariagertrude/src/test/java/com/inf2fm/pizzariagertrude/PizzariagertrudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inf2fm.pizzariagertrude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariagertrudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
